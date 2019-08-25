ffbc3b6cdb5e74712947d9a46d9f1f43b6940896
79laowang <edward.wang@163.com>
2019-08-26 00:37:09 +0800

    A script to hide/unhide PCI/PCIe device for KVM.(Using 'pci_stub' driver)

---
+++ pcistub.sh
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------------
+# File Name:   pcistub.sh
+# Purpose: hide and unhide a PCI/PCIe device for KVM pass-through
+#
+# Author:      Ke Wang
+#
+# Created:     2019-08-26
+# Copyright:   (c) Ke Wang 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+hide_dev=0
+unhide_dev=0
+driver=0
+
+# check if the device exists
+dev_exist(){
+	:
+}
+
+main(){
+    :
+}
+
+#---------------- Main Program --------------
+main "$@"
