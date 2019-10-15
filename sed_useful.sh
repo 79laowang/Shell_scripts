09146adfe5c67ad3f6b803b94b4ba5b75deaa6c2
79laowang <50512621+79laowang@users.noreply.github.com>
2019-10-15 15:06:47 +0800

    sed add or delete charactor at begining and tail of line

--- sed_useful.sh
+++ sed_useful.sh
@@ -9,3 +9,20 @@ echo $str | sed 's/[a-z]/\U&/g'
 # \L& is used to replace these uppercase letters with the lowercase version. 
 str="HELLO WORLD!"
 echo $str | sed 's/[A-Z]/\L&/g'
+
+# ------------
+cat > tmp.txt <<EOF
+12345_test6789
+12345_test6789
+123456789
+EOF
+
+#Delete first charactor of each line
+sed 's/.//' tmp.txt
+
+#Delete last charactors of each line with matched pattern
+sed '/test/s/..$//g' tmp.txt
+
+#Add a word after tail of matched pattern
+sed 's/_test/&log/g' tmp.txt
+
