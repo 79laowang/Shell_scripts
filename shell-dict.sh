b27c70aee3ea8c45132b59ff0973b48f1cf838de
79laowang <50512621+79laowang@users.noreply.github.com>
2019-10-16 11:20:05 +0800

    Add more operations

--- shell-dict.sh
+++ shell-dict.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-declare -A arr
+main(){
+    declare -A dict
 
-arr["key1"]=val1
+    dict["key1"]=val1
+    dict+=( ["key2"]=val2 ["key3"]=val3 )
+    # Iterate dictionary and print keys- values.
+    for key in ${!dict[@]}; do
+        echo "${key}:${dict[${key}]}"
+    done
+    dict["key1"]=val11 # modify a key's value
+    echo "dict:key1->${dict["key1"]}"
+    echo ${dict[@]} # get all values
+    echo ${!dict[*]} # get all keys
+    unset dict[key1]  # lets delete key1
+    echo ${dict[*]} # get all values
+}
 
-arr+=( ["key2"]=val2 ["key3"]=val3 )
-
-for key in ${!arr[@]}; do
-    echo ${key} ${arr[${key}]}
-done
+#---------------- Main Program --------------
+main "$@"
